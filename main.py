61777ca22aca3e777bccbaf4f9209f26df718f17
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-06-23 10:26:04 +0200

    Building and running the complete LangGraph agent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from graph.graph import app
 
 if __name__ == "__main__":
     print("Agentic RAG flows")
+    print(app.invoke(input={'question': 'what is agent memory?'}))
